f241a6ad4d4065f7ad7ff380b4df0a2053c6797b
Serg Kryvonos <serg.kryvonos@qt.io>
2023-09-29 12:50:37 +0200

    Move build qt pro command to its own file
    Change-Id: Ia73dab716b7013b4687f0f7b9b7fa5e8a2cc2ad2
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

---
+++ src/commands/build-qt-pro.ts
@@ -0,0 +1,133 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+import * as path from 'path';
+import * as vscode from 'vscode';
+import * as child_process from 'child_process';
+import * as qtpath from '../util/get-qt-paths';
+
+/**
+ * This file contains the implementation of the 'vscode-qt-tools.loadAndBuildQtProject' command.
+ * The command is registered in the 'src/extension.ts' file and moved here for better modularity.
+ */
+
+/**
+ * The 'loadAndBuildQtProject' command loads and builds a Qt project.
+ * @param context - The extension context.
+ * @returns The disposable for the command.
+ */
+async function loadAndBuildQtProject() {
+  function gotSelectedQt(selectedQtPath: string) {
+    // Get list of all .pro files in the workspace folders recursively
+    qtpath.findFilesInWorkspace('**/*.pro').then((proFiles: string[]) => {
+      if (proFiles.length === 0) {
+        vscode.window.showWarningMessage(
+          'Unable to locate Qt project .pro files. Consider using CMake build instead.'
+        );
+      } else {
+        // Show a quick pick dialog with the .pro files as options
+        vscode.window
+          .showQuickPick(
+            proFiles.map((uri) => {
+              return uri.toString();
+            }),
+            { placeHolder: 'Select a .pro file to load' }
+          )
+          .then((selectedProFile) => {
+            if (selectedProFile) {
+              // Set up a configure step with the default Qt version
+              const configureCommand = path.join(
+                selectedQtPath || '',
+                'bin',
+                `qmake ${selectedProFile}`
+              );
+
+              // Create an output channel for QMake output
+              const outputChannel =
+                vscode.window.createOutputChannel('QMake/Build');
+              outputChannel.show();
+
+              // Execute the configure step for the selected .pro file and show the output in the output window
+              const childProcess = child_process.exec(configureCommand, {
+                cwd: path.dirname(selectedProFile),
+                env: process.env
+              });
+              childProcess.on('close', (code) => {
+                if (code === 0) {
+                  // The configure step was successful, show the output in the output window
+                  outputChannel.appendLine('Configure step successful.');
+
+                  // Set up a build step that works with that Qt version
+                  let buildCommand;
+                  if (process.platform === 'win32') {
+                    // Use jom/mingw32-make on Windows
+                    buildCommand = 'jom';
+                  } else {
+                    // Use make on Linux/macOS
+                    buildCommand = 'make';
+                  }
+                  const childProcess = child_process.exec(buildCommand, {
+                    cwd: path.dirname(selectedProFile),
+                    env: process.env
+                  });
+                  childProcess.on('close', (code) => {
+                    if (code === 0) {
+                      // The configure step was successful, show the output in the output window
+                      outputChannel.appendLine('Build step successful.');
+                    } else {
+                      // The configure step failed, show the output in the output window
+                      outputChannel.appendLine('Build step failed.');
+                    }
+                  });
+                  childProcess.stdout?.on('data', (data) => {
+                    outputChannel.appendLine(data);
+                  });
+                  childProcess.stderr?.on('data', (data) => {
+                    outputChannel.appendLine(data);
+                  });
+                } else {
+                  // The configure step failed, show the output in the output window
+                  outputChannel.appendLine('Configure step failed.');
+                }
+              });
+              childProcess.stdout?.on('data', (data) => {
+                outputChannel.appendLine(data);
+              });
+              childProcess.stderr?.on('data', (data) => {
+                outputChannel.appendLine(data);
+              });
+            }
+          });
+      }
+    });
+  }
+
+  // Get the current configuration
+  const config = vscode.workspace.getConfiguration('vscode-qt-tools');
+  let defaultQt = config.get('defaultQt') as string;
+
+  if (defaultQt === undefined) {
+    // Call 'vscode-qt-tools.pickDefaultQt' command to ensure a default Qt version is set
+    vscode.commands.executeCommand('vscode-qt-tools.pickDefaultQt').then(() => {
+      // Get the current configuration
+      defaultQt = config.get('defaultQt') as string;
+      if (defaultQt === undefined) {
+        vscode.window.showWarningMessage(
+          'Unable to locate Qt. Please, use "Qt: Register Qt Installation" command to locate your Qt installation and try again.'
+        );
+      } else {
+        gotSelectedQt(defaultQt);
+      }
+    });
+  } else {
+    gotSelectedQt(defaultQt);
+  }
+}
+
+// Function to register the command
+export function registerLoadAndBuildQtProjectCommand() {
+  return vscode.commands.registerCommand(
+    'vscode-qt-tools.loadAndBuildQtProject',
+    loadAndBuildQtProject
+  );
+}
--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path';
-import * as child_process from 'child_process';
-import * as qtpath from './util/get-qt-paths';
 import { registerQtCommand } from './commands/register-qt-path';
 import { registerPickDefaultQtCommand } from './commands/pick-default-qt';
 import { registerDetectQtCMakeProjectCommand } from './commands/detect-qt-cmake';
+import { registerLoadAndBuildQtProjectCommand } from './commands/build-qt-pro';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -50,119 +48,9 @@ export function activate(context: vscode.ExtensionContext) {
   // Add a new command to detect if the opened project is a Qt project that uses CMake
   const detectQtCMakeProjectDisposable = registerDetectQtCMakeProjectCommand();
 
-  // Add a new command to load and build existing Qt projects that use QMake
-  const loadAndBuildQtProjectDisposable = vscode.commands.registerCommand(
-    'vscode-qt-tools.loadAndBuildQtProject',
-    () => {
-      function gotSelectedQt(selectedQtPath: string) {
-        // Get list of all .pro files in the workspace folders recursively
-        qtpath.findFilesInWorkspace('**/*.pro').then((proFiles: string[]) => {
-          if (proFiles.length === 0) {
-            vscode.window.showWarningMessage(
-              'Unable to locate Qt project .pro files. Consider using CMake build instead.'
-            );
-          } else {
-            // Show a quick pick dialog with the .pro files as options
-            vscode.window
-              .showQuickPick(
-                proFiles.map((uri) => {
-                  return uri.toString();
-                }),
-                { placeHolder: 'Select a .pro file to load' }
-              )
-              .then((selectedProFile) => {
-                if (selectedProFile) {
-                  // Set up a configure step with the default Qt version
-                  const configureCommand = path.join(
-                    selectedQtPath || '',
-                    'bin',
-                    `qmake ${selectedProFile}`
-                  );
-
-                  // Create an output channel for QMake output
-                  const outputChannel =
-                    vscode.window.createOutputChannel('QMake/Build');
-                  outputChannel.show();
-
-                  // Execute the configure step for the selected .pro file and show the output in the output window
-                  const childProcess = child_process.exec(configureCommand, {
-                    cwd: path.dirname(selectedProFile),
-                    env: process.env
-                  });
-                  childProcess.on('close', (code) => {
-                    if (code === 0) {
-                      // The configure step was successful, show the output in the output window
-                      outputChannel.appendLine('Configure step successful.');
-
-                      // Set up a build step that works with that Qt version
-                      let buildCommand;
-                      if (process.platform === 'win32') {
-                        // Use jom/mingw32-make on Windows
-                        buildCommand = 'jom';
-                      } else {
-                        // Use make on Linux/macOS
-                        buildCommand = 'make';
-                      }
-                      const childProcess = child_process.exec(buildCommand, {
-                        cwd: path.dirname(selectedProFile),
-                        env: process.env
-                      });
-                      childProcess.on('close', (code) => {
-                        if (code === 0) {
-                          // The configure step was successful, show the output in the output window
-                          outputChannel.appendLine('Build step successful.');
-                        } else {
-                          // The configure step failed, show the output in the output window
-                          outputChannel.appendLine('Build step failed.');
-                        }
-                      });
-                      childProcess.stdout?.on('data', (data) => {
-                        outputChannel.appendLine(data);
-                      });
-                      childProcess.stderr?.on('data', (data) => {
-                        outputChannel.appendLine(data);
-                      });
-                    } else {
-                      // The configure step failed, show the output in the output window
-                      outputChannel.appendLine('Configure step failed.');
-                    }
-                  });
-                  childProcess.stdout?.on('data', (data) => {
-                    outputChannel.appendLine(data);
-                  });
-                  childProcess.stderr?.on('data', (data) => {
-                    outputChannel.appendLine(data);
-                  });
-                }
-              });
-          }
-        });
-      }
-
-      // Get the current configuration
-      const config = vscode.workspace.getConfiguration('vscode-qt-tools');
-      let defaultQt = config.get('defaultQt') as string;
-
-      if (defaultQt === undefined) {
-        // Call 'vscode-qt-tools.pickDefaultQt' command to ensure a default Qt version is set
-        vscode.commands
-          .executeCommand('vscode-qt-tools.pickDefaultQt')
-          .then(() => {
-            // Get the current configuration
-            defaultQt = config.get('defaultQt') as string;
-            if (defaultQt === undefined) {
-              vscode.window.showWarningMessage(
-                'Unable to locate Qt. Please, use "Qt: Register Qt Installation" command to locate your Qt installation and try again.'
-              );
-            } else {
-              gotSelectedQt(defaultQt);
-            }
-          });
-      } else {
-        gotSelectedQt(defaultQt);
-      }
-    }
-  );
+  // Register the 'vscode-qt-tools.loadAndBuildQtProject' command
+  const loadAndBuildQtProjectDisposable =
+    registerLoadAndBuildQtProjectCommand();
 
   context.subscriptions.push(
     proFileDisposable,
