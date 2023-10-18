7e3dde7049dea0f32f7f44d8bee46e960126d747
Serg Kryvonos <serg.kryvonos@qt.io>
2023-10-18 14:24:50 +0200

    Add extension activation time log
    Measure extension activation time (see debug console).
    
    Task-number: VSCODEEXT-48
    Change-Id: Icb8e0f25d44e749d8641a281977bb95ed5adb541
    Reviewed-by: Orkun Tokdemir <orkun.tokdemir@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { performance } from 'perf_hooks';
 import {
   checkForQtInstallationsUpdates,
   registerQtCommand
@@ -18,6 +19,8 @@ import { registerQdocFile } from './commands/file-ext-qdoc';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const activateStart = performance.now();
+
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
@@ -55,6 +58,12 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   checkForQtInstallationsUpdates();
+
+  const activateEnd = performance.now();
+  const activationTime = activateEnd - activateStart;
+  console.log(
+    `Done activating plugin ${context.extension.id}, took ${activationTime}ms`
+  );
 }
 
 // This method is called when your extension is deactivated
