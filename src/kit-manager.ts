qt-labs/vscodeext

commit 6f8309031b797df47631711ea640367bca925e73

Fix kit name mangling when the basename is not `Qt`
If a basename is selected different from `qt`, `mangleQtInstallation()`
generates a name with the full path. This patch fixes that.
"qtFolder"'s basename can be used in kit names.

Example
qtFolder:"usr/home/Qt_custom" -> Kit name:"Qt_custom-6.6.2-gcc"

Change-Id: I52956e1806e56fa66aeeb3de360fbfb7cb958784
Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

src/kit-manager.ts
@@ -242,10 +242,11 @@ export class KitManager {
       : [];
     project
       ? await this.updateQtInstallations(
+          currentQtFolder,
           newQtInstallations,
           project.getFolder()
         )
-      : await this.updateQtInstallations(newQtInstallations);
+      : await this.updateQtInstallations(currentQtFolder, newQtInstallations);
   }
 
   private async checkForGlobalQtInstallations() {
@@ -308,7 +309,7 @@ export class KitManager {
         console.log(`Found ${qtInstallation.length} Qt installation(s).`);
       }
     }
-    await this.updateQtInstallations(qtInstallation, workspaceFolder);
+    await this.updateQtInstallations(qtFolder, qtInstallation, workspaceFolder);
     if (workspaceFolder) {
       await this.getProject(workspaceFolder)
         ?.getStateManager()
@@ -334,6 +335,7 @@ export class KitManager {
   }
 
   private async *generateCMakeKitsOfQtInstallationPath(
+    qtFolder: string,
     installation: string,
     loadedCMakeKits: Kit[]
   ) {
@@ -353,7 +355,7 @@ export class KitManager {
     }
 
     let newKit: Kit = {
-      name: qtPath.mangleQtInstallation(installation),
+      name: qtPath.mangleQtInstallation(qtFolder, installation),
       environmentVariables: {
         VSCODE_QT_FOLDER: installation,
         PATH: qtPathEnv
@@ -383,7 +385,7 @@ export class KitManager {
         const msvcKitsClone: Kit[] = JSON.parse(
           JSON.stringify(loadedCMakeKits)
         ) as Kit[];
-        yield* this.generateMsvcKits(newKit, msvcKitsClone);
+        yield* this.generateMsvcKits(qtFolder, newKit, msvcKitsClone);
         return;
       } else if (platform.startsWith('mingw')) {
         platform = os.platform();
@@ -427,11 +429,15 @@ export class KitManager {
     yield newKit;
   }
 
-  private async cmakeKitsFromQtInstallations(qtInstallations: string[]) {
+  private async cmakeKitsFromQtInstallations(
+    qtFolder: string,
+    qtInstallations: string[]
+  ) {
     const loadedCMakeKits = await this.loadCMakeKitsFileJSON();
     const kits = [];
     for (const installation of qtInstallations)
       for await (const kit of this.generateCMakeKitsOfQtInstallationPath(
+        qtFolder,
         installation,
         loadedCMakeKits
       ))
@@ -440,11 +446,14 @@ export class KitManager {
   }
 
   private async updateQtInstallations(
+    qtFolder: string,
     qtInstallations: string[],
     workspaceFolder?: vscode.WorkspaceFolder
   ) {
-    const newGeneratedKits =
-      await this.cmakeKitsFromQtInstallations(qtInstallations);
+    const newGeneratedKits = await this.cmakeKitsFromQtInstallations(
+      qtFolder,
+      qtInstallations
+    );
     await this.updateCMakeKitsJson(newGeneratedKits, workspaceFolder);
 
     if (workspaceFolder) {
@@ -547,7 +556,11 @@ export class KitManager {
     return generator ? generator : CMakeDefaultGenerator;
   }
 
-  private *generateMsvcKits(newKit: Kit, loadedCMakeKits: Kit[]) {
+  private *generateMsvcKits(
+    qtFolder: string,
+    newKit: Kit,
+    loadedCMakeKits: Kit[]
+  ) {
     const msvcInfoMatch =
       newKit.visualStudio?.match(KitManager.MsvcInfoRegexp) ??
       newKit.visualStudio?.match(KitManager.MsvcInfoNoArchRegexp);
@@ -578,6 +591,7 @@ export class KitManager {
     });
     for (const kit of msvcKitsWithArchitectureMatch) {
       kit.name = qtPath.mangleQtInstallation(
+        qtFolder,
         newKit.name + ' - ' + (kit.name || '')
       );
       if (kit.preferredGenerator) {

src/util/get-qt-paths.ts
@@ -38,13 +38,14 @@ function qtToolsDirByQtRootDir(qtRootDir: string): string {
   return path.normalize(path.join(qtRootDir, 'Tools'));
 }
 
-export function mangleQtInstallation(installation: string): string {
+export function mangleQtInstallation(
+  qtFolder: string,
+  installation: string
+): string {
+  installation = installation.replace(qtFolder, '');
   const pathParts = installation.split(/[/\\:]+/).filter((n) => n);
-  const qtIdx = Math.max(
-    0,
-    pathParts.findIndex((s) => s.toLowerCase() == 'qt')
-  );
-  return pathParts.slice(qtIdx).join('-');
+  pathParts.unshift(path.basename(qtFolder));
+  return pathParts.slice().join('-');
 }
 
 async function locateQmakeExeFilePath(selectedQtPath: string) {

commit 1f76f157285310f69c163f341dc351f042dbaaf3

Fix Double qtFolder name in Qt MSVC kits
6f8309031b797df47631711ea640367bca925e73 introduced this bug when
`qtFolder` is passed to all `mangleQtInstallation()` functions. This
commits reverts it for msvc kits.

Change-Id: I74e3ab7730a43bbf5b37977d4488528e056f789a
Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

src/kit-manager.ts
@@ -589,8 +589,7 @@ export class KitManager {
       return isArchMatch && versions.compareVersions(version, vsYear) >= 0;
     });
     for (const kit of msvcKitsWithArchitectureMatch) {
-      kit.name = qtPath.mangleQtInstallation(
-        qtFolder,
+      kit.name = qtPath.mangleMsvcKitName(
         newKit.name + ' - ' + (kit.name || '')
       );
       if (kit.preferredGenerator) {

src/util/get-qt-paths.ts
@@ -50,6 +50,15 @@ export function mangleQtInstallation(
   return pathParts.slice().join('-');
 }
 
+export function mangleMsvcKitName(installation: string): string {
+  const pathParts = installation.split(/[/\\:]+/).filter((n) => n);
+  const qtIdx = Math.max(
+    0,
+    pathParts.findIndex((s) => s.toLowerCase() == 'qt')
+  );
+  return pathParts.slice(qtIdx).join('-');
+}
+
 async function locateQmakeExeFilePath(selectedQtPath: string) {
   const bin = path.join(selectedQtPath, 'bin');
   const qmakeExePath = path.join(bin, QmakeFileName);