5705c210dc60c0424ba4955848e6e872c4072b4c
Orkun Tokdemir <orkun.tokdemir@qt.io>
2024-04-18 17:45:26 +0200

    Style: Include `.eslintrc.cjs` to prettier
    * Remove non-existent file from .prettierignore
    
    Change-Id: I1413acde6504338717c0579f80bacb2bea900506
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,27 +3,27 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/strict-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked'
   ],
-  "rules": {
-    "@typescript-eslint/no-shadow": "error",
-    "@typescript-eslint/prefer-readonly": "error",
-    "@typescript-eslint/return-await": "error",
-    "@typescript-eslint/no-loop-func": "error",
-    "@typescript-eslint/no-unnecessary-qualifier": "error",
-    "@typescript-eslint/prefer-find": "error",
-    "@typescript-eslint/no-require-imports": "error",
-    "@typescript-eslint/restrict-plus-operands": "error",
-    "@typescript-eslint/switch-exhaustiveness-check": "error",
-    "@typescript-eslint/no-useless-empty-export": "error"
+  rules: {
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/prefer-readonly': 'error',
+    '@typescript-eslint/return-await': 'error',
+    '@typescript-eslint/no-loop-func': 'error',
+    '@typescript-eslint/no-unnecessary-qualifier': 'error',
+    '@typescript-eslint/prefer-find': 'error',
+    '@typescript-eslint/no-require-imports': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    '@typescript-eslint/no-useless-empty-export': 'error'
   },
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
   root: true,
-  ignorePatterns: ['out', 'src/test'],
+  ignorePatterns: ['out', 'src/test']
 };
