49a6d8d96f3f3953d218a39b13c52967f23cd98a
Orkun Tokdemir <orkun.tokdemir@qt.io>
2024-06-28 12:58:51 +0200

    Remove redundant slashes
    Change-Id: Ieabd204d54976c2b31972eb736bc3bb70d7093a4
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>
    Reviewed-by: Ben Cho <ben.cho@qt.io>

--- src/tasks/wasm-start.ts
+++ src/tasks/wasm-start.ts
@@ -1,5 +1,5 @@
-// // Copyright (C) 2024 The Qt Company Ltd.
-// // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
 
 import * as vscode from 'vscode';
 
