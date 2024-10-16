733436b53c666febdf28cb3ec7a06dd60d322b44
Orkun Tokdemir <orkun.tokdemir@qt.io>
2024-10-16 16:12:35 +0200

    qt-core: Add msvc version parsing for qtpaths
    * Factor out the common code to find `qconfig.pri`
    
    Change-Id: I2059e3369a18e5e50c1335e4963bb1aea44c408f
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- qt-core/src/api.ts
+++ qt-core/src/api.ts
@@ -17,6 +17,11 @@ import {
 
 const logger = createLogger('api');
 
+interface MSVCVersion {
+  major: number;
+  minor: number;
+  patch: number;
+}
 export class CoreAPIImpl implements CoreAPI {
   private readonly _configs = new Map<
     vscode.WorkspaceFolder | string,
@@ -52,7 +57,18 @@ export class CoreAPIImpl implements CoreAPI {
     }
   }
 
-  private static obtainArch(qtInfo: QtInfo) {
+  private static obtainArch(content: string) {
+    const keysToCheck = ['QT_ARCHS', 'QT_TARGET_ARCH', 'QT_ARCH'];
+    for (const k of keysToCheck) {
+      const match = content.match(new RegExp(`${k}\\s*\\=\\s*(.*)`));
+      if (match) {
+        return match[1];
+      }
+    }
+    return undefined;
+  }
+
+  private static getQConfigPriContent(qtInfo: QtInfo) {
     const keysToCheckForQConfigPri = [
       'QT_HOST_PATH',
       'QT_INSTALL_PREFIX',
@@ -68,18 +84,32 @@ export class CoreAPIImpl implements CoreAPI {
       }
       const qconfigPriPath = path.join(p, 'mkspecs', 'qconfig.pri');
       if (fs.existsSync(qconfigPriPath)) {
-        const content = fs.readFileSync(qconfigPriPath, 'utf8');
-        const keysToCheck = ['QT_ARCHS', 'QT_TARGET_ARCH', 'QT_ARCH'];
-        for (const k of keysToCheck) {
-          const match = content.match(new RegExp(`${k}\\s*\\=\\s*(.*)`));
-          if (match) {
-            return match[1];
-          }
-        }
+        return fs.readFileSync(qconfigPriPath, 'utf8');
       }
     }
 
     return undefined;
+  }
+  private static obtainMSVCVersions(content: string): MSVCVersion {
+    const ret: MSVCVersion = { major: 0, minor: 0, patch: 0 };
+    const keysToCheck = [
+      'QT_MSVC_MAJOR_VERSION',
+      'QT_MSVC_MINOR_VERSION',
+      'QT_MSVC_PATCH_VERSION'
+    ];
+    for (const k of keysToCheck) {
+      const match = content.match(new RegExp(`${k}\\s*\\=\\s*(.*)`));
+      if (match) {
+        if (k === 'QT_MSVC_MAJOR_VERSION') {
+          ret.major = parseInt(match[1] ?? '-1');
+        } else if (k === 'QT_MSVC_MINOR_VERSION') {
+          ret.minor = parseInt(match[1] ?? '-1');
+        } else if (k === 'QT_MSVC_PATCH_VERSION') {
+          ret.patch = parseInt(match[1] ?? '-1');
+        }
+      }
+    }
+    return ret;
   }
 
   update(message: QtWorkspaceConfigMessage) {
@@ -146,15 +176,21 @@ export class CoreAPIImpl implements CoreAPI {
         result.set(key.trim(), value.trim());
       }
     }
-    const arch = CoreAPIImpl.obtainArch(result);
-    if (arch) {
-      result.set('ARCH', arch);
-    } else {
-      logger.warn(
-        `Could not determine architecture for ${qtAdditionalPath.path}`
-      );
+    const qconfigPriContent = CoreAPIImpl.getQConfigPriContent(result);
+    if (qconfigPriContent) {
+      const arch = CoreAPIImpl.obtainArch(qconfigPriContent);
+      if (arch) {
+        result.set('ARCH', arch);
+      } else {
+        logger.warn(
+          `Could not determine architecture for ${qtAdditionalPath.path}`
+        );
+      }
+      const msvcVersion = CoreAPIImpl.obtainMSVCVersions(qconfigPriContent);
+      result.set('MSVC_MAJOR_VERSION', msvcVersion.major.toString());
+      result.set('MSVC_MINOR_VERSION', msvcVersion.minor.toString());
+      result.set('MSVC_PATCH_VERSION', msvcVersion.patch.toString());
     }
-
     this._qtInfoCache.set(qtAdditionalPath, result);
     return result;
   }
