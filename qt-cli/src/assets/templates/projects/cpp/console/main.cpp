c0fac6707ea8bb1e597877927c33727a18fa5a1b
Joerg Bornemann <joerg.bornemann@qt.io>
2025-04-10 14:35:45 +0200

    qt-cli: Modernize the console template
    Don't add Qt5 support code in CMakeLists.txt and use more recent API.

--- qt-cli/src/assets/templates/projects/cpp/console/main.cpp
+++ qt-cli/src/assets/templates/projects/cpp/console/main.cpp
@@ -6,20 +6,20 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
 {{ if .useTranslation }}
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
         const QString baseName = "{{ .name }}_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
+            app.installTranslator(&translator);
             break;
         }
     }
 {{- end }}
     // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
+    // Call app.quit() or app.exit() to quit the application.
     // A not very useful example would be including
     // #include <QTimer>
     // near the top of the file and calling
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     // which quits the application after 5 seconds.
 
     // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+    // to app.exec() or use the Non-Qt Plain C++ Application template.
 
-    return a.exec();
+    return app.exec();
 }
