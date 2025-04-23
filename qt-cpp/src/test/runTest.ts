25f8d4696b4d7beb3c14a647e5ef08e2662da711
Lucie Gérard <lucie.gerard@qt.io>
2025-04-23 14:11:11 +0200

    qt-cpp: Add installing extension depedencies in test Host

--- qt-cpp/src/test/runTest.ts
+++ qt-cpp/src/test/runTest.ts
@@ -1,6 +1,12 @@
+import * as cp from 'child_process';
 import * as path from 'path';
+const packageJson = require('../../package.json');
 
-import { runTests } from '@vscode/test-electron';
+import {
+  downloadAndUnzipVSCode,
+  resolveCliArgsFromVSCodeExecutablePath,
+  runTests
+} from '@vscode/test-electron';
 
 async function main() {
   try {
@@ -11,6 +17,23 @@ async function main() {
     // The path to the extension test script
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
+
+    const vscodeExecutablePath = await downloadAndUnzipVSCode();
+    const [cli, ...args] =
+      resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    if (packageJson.extensionDependencies) {
+      for (const extensionId of packageJson.extensionDependencies) {
+        cp.spawnSync(
+          <string>cli,
+          [...args, '--install-extension', extensionId],
+          {
+            encoding: 'utf-8',
+            stdio: 'inherit'
+          }
+        );
+      }
+    }
 
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
