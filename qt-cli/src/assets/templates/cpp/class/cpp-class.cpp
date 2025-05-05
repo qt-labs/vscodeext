4d525836a7af4ab8ceda465d96f7e6cc50a7ad7c
Ben Cho <ben.cho@qt.io>
2025-05-05 14:15:50 +0200

    qt-cli: Simplify templates and add metadata
    Simplify templates by removing unnecessary details.
    Set default Qt version to 6.5 for Qt Quick project, better aligned
    with recent Qt versions.
    Add metadata - title, description - to all templates for frontend use.

--- qt-cli/src/assets/templates/cpp/class/cpp-class.cpp
+++ qt-cli/src/assets/templates/cpp/class/cpp-class.cpp
@@ -1,54 +1,6 @@
-{{ if .useQSharedData }}#include <utility>{{ end }}
 #include "{{ .headerFileName }}"
 
-{{ .namespaceOpening }}
-
-{{ if .useQSharedData }}
-class {{ .sharedDataClass }}: public QSharedData
-{
-public:
-};
-{{ end }}
-
-{{- if .parentClass }}
-{{ .className }}::{{ .className }}({{ .parentClass }} *parent)
-    : {{ .baseClass }}{parent}{{ if .useQSharedData }}, data(new {{ .sharedDataClass }}){{ end }}
-{{- else }}
-{{ .className }}::{{ .className }}(){{ if .useQSharedData }}: data(new {{ .sharedDataClass }}){{ end }}
-{{- end }}
+{{ .name }}::{{ .name }}({{ .parentClass }} *parent)
+    : {{ .baseClass }}{parent}
 {
 }
-
-{{ if .useQSharedData }}
-{{ .className }}::{{ .className }}(const {{ .className }} &rhs)
-    : data{rhs.data}
-{
-}
-
-{{ .className }}::{{ .className }}({{ .className }} &&rhs)
-    : data{std::move(rhs.data)}
-{
-}
-
-{{ .className }} &{{ .className }}::operator=(const {{ .className }} &rhs)
-{
-    if (this != &rhs)
-        data = rhs.data;
-
-    return *this;
-}
-
-{{ .className }} &{{ .className }}::operator=({{ .className }} &&rhs)
-{
-    if (this != &rhs)
-        data = std::move(rhs.data);
-
-    return *this;
-}
-
-{{ .className }}::~{{ .className }}()
-{
-}
-{{ end }}
-
-{{ .namespaceClosing }}
--- qt-cli/src/assets/templates/cpp/class/cpp-class.h
+++ qt-cli/src/assets/templates/cpp/class/cpp-class.h
@@ -1,61 +1,25 @@
 {{/* variables */}}
 {{ $macros := Qt.NewArray }}
-{{ $macros = (Qt.AppendIf $macros "Q_OBJECT" (or .isQObject (not (eq .qtBaseClass "")))) }}
-{{ $macros = (Qt.AppendIf $macros "QML_ELEMENT" (eq .qtBaseClass "QQuickItem")) }}
+{{ $macros = (Qt.Append $macros "Q_OBJECT") }}
+{{ $macros = (Qt.AppendIf $macros "QML_ELEMENT" (eq .baseClass "QQuickItem")) }}
 
 {{ $includes := Qt.NewArray }}
-{{ $includes = (Qt.AppendIf $includes (printf "<%s>" .qtBaseClass) (not (eq .qtBaseClass ""))) }}
-{{ $includes = (Qt.AppendIf $includes "<QSharedDataPointer>" .useQSharedData) }}
-{{ $includes = (Qt.AppendIf $includes (printf "\"%s.h\"" .customBaseClass) (not (eq .customBaseClass ""))) }}
+{{ $includes = (Qt.AppendIf $includes (printf "<%s>" .baseClass) (not (eq .baseClass ""))) }}
 
 {{/* contents */}}
-{{ if .usePragmaOnce }}
 #pragma once
-{{ else }}
-#ifndef {{ .includeGuardName }}
-#define {{ .includeGuardName }}
-{{ end }}
 
 {{ range $includes }}#include {{ . }}
 {{ end }}
 
-{{ .namespaceOpening }}
-
-{{ if .useQSharedData }}class {{ .sharedDataClass }};{{ end }}
-
-class {{ .className }}{{ if .baseClass }}: public {{ .baseClass }}{{ end }}
+class {{ .name }}{{ if .baseClass }}: public {{ .baseClass }}{{ end }}
 {
 {{ range $macros }}    {{ . }}
 {{ end }}
 {{- if $macros }}{{ printf "\n" }}{{ end -}}
 
 public:
-{{- if .parentClass }}
-    explicit {{ .className }}({{ .parentClass }} *parent = nullptr);
-{{- else }}
-    {{ .className }}();
-{{ end }}
+    explicit {{ .name }}({{ .parentClass }} *parent = nullptr);
 
-{{ if .useQSharedData }}
-    {{ .className }}(const {{ .className }} &);
-    {{ .className }}({{ .className }} &&);
-    {{ .className }} &operator=(const {{ .className }} &);
-    {{ .className }} &operator=({{ .className }} &&);
-    ~{{ .className }}();
-{{ end }}
-
-{{ if .isQObject }}
 Q_SIGALS:
-{{ end }}
-
-{{ if .useQSharedData }}
-private:
-    QSharedDataPointer<{{ .sharedDataClass }}> data;
-{{ end }}
 };
-
-{{ .namespaceClosing }}
-
-{{ if not .usePragmaOnce }}
-#endif // {{ .includeGuardName }}
-{{ end }}
