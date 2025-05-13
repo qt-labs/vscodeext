11982e7d6f9ac162367e94cf19185668d9f076be
Lucie Gérard <lucie.gerard@qt.io>
2025-05-13 12:55:48 +0200

    qt-cpp: Add testing cmake extension is installed
    Calling a cmake command

--- qt-cpp/src/test/suite/extension.test.ts
+++ qt-cpp/src/test/suite/extension.test.ts
@@ -1,9 +1,10 @@
 import * as assert from 'assert';
+import { expect } from 'chai';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+//import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
@@ -11,5 +12,13 @@ suite('Extension Test Suite', () => {
   test('Sample test', () => {
     assert.strictEqual([1, 2, 3].indexOf(5), -1);
     assert.strictEqual([1, 2, 3].indexOf(0), -1);
+  });
+
+  test('cmake extension test', async () => {
+    // The folder is invalid and there is no active CMake project. cmake.configure returns -2
+    expect(await vscode.commands.executeCommand('cmake.configure')).to.be.eq(
+      -2
+    );
+    // Error: command 'cmake.configure' not found if the cmake extension is not installed
   });
 });
