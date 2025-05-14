c78d5974076956f000bd4998079ecf80219be0da
Lucie Gérard <lucie.gerard@qt.io>
2025-05-14 11:26:47 +0200

    qt-cpp: Add license and copyright to test files
    Small clean up

--- qt-cpp/src/test/suite/index.ts
+++ qt-cpp/src/test/suite/index.ts
@@ -1,9 +1,5 @@
-// import * as path from 'path';
-// void path;
-// import Mocha from 'mocha';
-// void Mocha;
-// import * as glob from 'glob';
-// void glob;
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
 
 import * as path from 'path';
 import Mocha from 'mocha';
