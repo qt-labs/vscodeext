0331f658638a2ac01c79e1a76a97c97270e9c628
Lucie Gérard <lucie.gerard@qt.io>
2025-05-14 11:17:22 +0200

    qt-cpp: Add checking extension depedencies activation
    Removes cmake and qt-core previous individual tests

--- qt-cpp/src/test/suite/extension.test.ts
+++ qt-cpp/src/test/suite/extension.test.ts
@@ -5,7 +5,8 @@ import { expect } from 'chai';
 import * as vscode from 'vscode';
 // Ideally should not be hard coded but infered from the depedencies
 // listed in qt-cpp package.json extensionDependencies
-const corePackageJson = require('../../../../qt-core/package.json');
+//const corePackageJson = require('../../../../qt-core/package.json');
+const packageJson = require('../../../package.json');
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
@@ -16,30 +17,20 @@ suite('Extension Test Suite', () => {
   });
 
   suiteSetup(async function (this: Mocha.Context) {
-    // by activating qt-cpp, the extension depedencies (qt-core and cmake) are activated
-    // Needed for getCommands to see the extensions commands.
+    // activation of qt-cpp activates the extension depedencies (qt-core and cmake)
+    // activation needed for vscode.commands.getCommands to see the extensions commands
     await vscode.extensions.getExtension('theqtcompany.qt-cpp')?.activate();
     this.timeout(10000);
   });
 
-  test('cmake extension test', async () => {
-    // The folder is invalid and there is no active CMake project. cmake.configure returns -2
-    expect(await vscode.commands.executeCommand('cmake.configure')).to.be.eq(
-      -2
-    );
-    // Error: command 'cmake.configure' not found if the cmake extension is not installed
-  });
-
-  test('Qt core extension test commands are seen', async () => {
-    const vscodeCommands = vscode.commands.getCommands(true);
-
-    if (corePackageJson.contributes.commands) {
-      // Listing qt-core commands
-      for (const command of corePackageJson.contributes.commands) {
-        let string_com: string = command.command;
-        console.log(string_com);
-        expect((await vscodeCommands).includes(string_com)).to.be.eq(true);
+  test('Extension depedencies are active', async () => {
+    if (packageJson.extensionDependencies) {
+      for (const extensionId of packageJson.extensionDependencies) {
+        expect(vscode.extensions.getExtension(extensionId)?.isActive).to.be.eq(
+          true
+        );
       }
     }
   });
+
 });
