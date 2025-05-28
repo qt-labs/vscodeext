e70b1f9ca833b7ec14547af1ef05da9d328b8d18
Lucie Gérard <lucie.gerard@qt.io>
2025-05-28 15:25:40 +0200

    qt-ui: Add integration test
    - Test structure and basic test in place

--- qt-ui/esbuild.mjs
+++ qt-ui/esbuild.mjs
@@ -24,6 +24,23 @@ const extensionConfig = {
   external: ['vscode']
 };
 
+// Config for extension test source code (to be run in a Node-based context)
+/** @type BuildOptions */
+const extensionTestConfig = {
+  ...baseConfig,
+  platform: 'node',
+  mainFields: ['module', 'main'],
+  tsconfig: './tsconfig.json',
+  format: 'cjs',
+  entryPoints: [
+    './src/test/runTest.ts',
+    './src/test/suite/index.ts',
+    './src/test/suite/extension.test.ts'
+  ],
+  outdir: './out/test/',
+  external: ['vscode', './reporters/parallel-buffered', './worker.js']
+};
+
 // Config for webview source code (to be run in a web-based context)
 /** @type BuildOptions */
 const webviewConfig = {
@@ -83,10 +100,15 @@ await execCmd('npx tsc --noEmit').then(
       await webCtx.dispose();
       console.log('[watch] build finished');
     } else {
-      // Build extension and webview code
-      await build(extensionConfig);
-      await build(webviewConfig);
-      console.log('build complete');
+      if (args.includes('--test')) {
+        await build(extensionTestConfig);
+        console.log('[tests] build complete');
+      } else {
+        // Build extension and webview code
+        await build(extensionConfig);
+        await build(webviewConfig);
+        console.log('build complete');
+      }
     }
   } catch (err) {
     process.stderr.write(err.stderr);
