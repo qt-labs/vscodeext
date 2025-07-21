879a42bbe75988a68c78163d42762f1f4edc489f
Lucie Gérard <lucie.gerard@qt.io>
2025-07-21 20:06:22 +0200

    Set helper for extensions test in qt-lib

--- qt-lib/src/index.ts
+++ qt-lib/src/index.ts
@@ -7,3 +7,5 @@ export * from './state';
 export * from './telemetry';
 export * from './color-provider';
 export * from './configuration-resolver';
+export * from './test-helper';
+export * from './test-constants';
