7010c85d9d2c37df45966f70844f69b1802a3e29
Orkun Tokdemir <orkun.tokdemir@qt.io>
2025-07-22 17:24:48 +0200

    License: Improve generating licenses
    * Use the MIT license template when the license is MIT and not be able
    to find the license file.
    * Ignore fetch errors when trying to get the license file from a remote
    repository.

--- scripts/generate_licenses.ts
+++ scripts/generate_licenses.ts
@@ -84,7 +84,7 @@ async function main() {
         'LICENSE.txt',
         'License.txt',
         'LICENSE.TXT',
-        'MIT-LICENSE.txt',
+        'MIT-LICENSE.txt'
       ];
       const repo = license.repository.replace(
         'github.com',
@@ -94,19 +94,27 @@ async function main() {
       let found = false;
       for (const possibleLicenseFileName of possibleLicenseFileNames) {
         for (const possibleBranch of possibleBranches) {
-          const response = await fetch(
-            `${repo}/${possibleBranch}/${possibleLicenseFileName}`
-          );
-          if (response.ok) {
-            const licenseFile = await response.text();
-            appendLicense(licenseFile);
-            found = true;
-            break;
+          try {
+            const response = await fetch(
+              `${repo}/${possibleBranch}/${possibleLicenseFileName}`
+            );
+            if (response.ok) {
+              const licenseFile = await response.text();
+              appendLicense(licenseFile);
+              found = true;
+              break;
+            }
+          } catch (error) {
+            // Ignore errors, continue to next possible file or branch
           }
         }
       }
       if (!found) {
-        throw new Error(`Cannot find license file for ${name} in ${repo}`);
+        if (license.licenses === 'MIT') {
+          appendLicense(MIT_LICENSE);
+        } else {
+          throw new Error(`Cannot find license file for ${name} in ${repo}`);
+        }
       }
     } else {
       const licenseFile = fs.readFileSync(license.licenseFile, 'utf-8');
@@ -120,3 +128,23 @@ async function main() {
 }
 
 void main();
+
+const MIT_LICENSE = `This software is released under the MIT license:
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+`;
