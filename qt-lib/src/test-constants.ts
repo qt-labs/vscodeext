d1862107d23695253292bbda80503e7f7eaa0d3b
Orkun Tokdemir <orkun.tokdemir@qt.io>
2025-09-15 14:12:35 +0200

    qt-cpp: qt-qml: qt-ui: Obtain qt-core version from package.json of qt-core extension
    In the previous implementation, when we bumped the version of qt-core
    extension, we were also supposed to update the version in qt-lib.
    Instead of doing that, we can directly read the version from qt-core's
    package.json. This way, we ensure that qt-lib always uses the correct
    version of qt-core without needing to manually update it.

--- qt-lib/src/test-constants.ts
+++ qt-lib/src/test-constants.ts
@@ -1,5 +1,26 @@
 // Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
 
-// Path to the local qt-core extension to be used during testing
-export const localQtCore = '../../../qt-core/out/qt-core-1.9.0.vsix';
+import * as path from 'path';
+import * as fs from 'fs';
+
+interface RootPackage {
+  version: string;
+}
+
+function getExtensionVersion(extensionRoot: string): string {
+  const packageJsonPath = path.join(extensionRoot, 'package.json');
+  const packageJson = JSON.parse(
+    fs.readFileSync(packageJsonPath, 'utf-8')
+  ) as RootPackage;
+  return packageJson.version;
+}
+
+export function getLocalQtCore() {
+  const qtcoreExtensionRoot = '../../../qt-core';
+  const packageVersion = getExtensionVersion(qtcoreExtensionRoot);
+  if (!packageVersion) {
+    throw new Error('Failed to get package version');
+  }
+  return `../../../qt-core/out/qt-core-${packageVersion}.vsix`;
+}
