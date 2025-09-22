f16470d7b25465f279878acfef88f09f17cf435d
Ben Cho <ben.cho@qt.io>
2025-09-22 11:45:55 +0200

    qt-ui: Refactor around UIProject
    - Centralize Qt Widgets Designer client handling
    - Extract UIProjectManager to clarify project management
    - Handle core and workspace events in one place
    - Simplify state and update flow
    - Remove unused setters and reorder functions

--- qt-ui/src/extension.ts
+++ qt-ui/src/extension.ts
@@ -6,72 +6,37 @@ import * as vscode from 'vscode';
 import {
   CoreAPI,
   getCoreApi,
-  QtWorkspaceConfigMessage,
-  QtWorkspaceType,
-  ProjectManager,
   createLogger,
   initLogger,
   telemetry
 } from 'qt-lib';
-import { UIEditorProvider } from '@/editors/ui/ui-editor';
-import { createUIProject, UIProject } from '@/project';
 import { EXTENSION_ID } from '@/constants';
 import { openWidgetDesigner } from '@/commands';
+import { UIProjectManager } from '@/project-manager';
+import { UIEditorProvider } from '@/editors/ui/ui-editor';
 
 const logger = createLogger('extension');
 
-export let projectManager: ProjectManager<UIProject>;
 export let coreAPI: CoreAPI | undefined;
+export let projectManager: UIProjectManager;
 
 export async function activate(context: vscode.ExtensionContext) {
   initLogger(EXTENSION_ID);
-  telemetry.activate(context);
   logger.info(`Activating ${context.extension.id}`);
-  coreAPI = await getCoreApi();
-  if (!coreAPI) {
-    const err = 'Failed to get CoreAPI';
-    logger.error(err);
-    throw new Error(err);
-  }
+  telemetry.activate(context);
 
-  projectManager = new ProjectManager<UIProject>(context, createUIProject);
-  projectManager.onProjectAdded(async (project) => {
-    logger.info('Adding project:', project.folder.uri.fsPath);
+  await initCoreApi();
+  await initProjectManager(context);
 
-    project.workspaceType = coreAPI?.getValue<QtWorkspaceType>(
-      project.folder,
-      'workspaceType'
-    );
-    await project.tryToGetDesigner();
-  });
-  projectManager.onProjectRemoved((project) => {
-    logger.info('Project removed:', project.folder.uri.fsPath);
-  });
-  if (vscode.workspace.workspaceFolders !== undefined) {
-    for (const folder of vscode.workspace.workspaceFolders) {
-      const project = await createUIProject(folder, context);
-      projectManager.addProject(project);
-    }
-  }
-  coreAPI.onValueChanged((message) => {
-    logger.info('Received config change:', message.config as unknown as string);
-    void processMessage(message);
-  });
-  context.subscriptions.push(UIEditorProvider.register(context));
   context.subscriptions.push(
+    UIEditorProvider.register(context),
     vscode.commands.registerCommand(
       `${EXTENSION_ID}.openWidgetDesigner`,
       openWidgetDesigner
     )
   );
-  getConfigValues();
-  telemetry.sendEvent(`activated`);
-}
 
-function getConfigValues() {
-  for (const project of projectManager.getProjects()) {
-    void project.getConfigValues();
-  }
+  telemetry.sendEvent('activated');
 }
 
 export function deactivate() {
@@ -80,42 +45,16 @@ export function deactivate() {
   projectManager.dispose();
 }
 
-async function processMessage(message: QtWorkspaceConfigMessage) {
-  // check if workspace folder is a string
-  if (typeof message.workspaceFolder === 'string') {
-    return;
+async function initCoreApi() {
+  coreAPI = await getCoreApi();
+  if (!coreAPI) {
+    const msg = 'Failed to get CoreAPI';
+    logger.error(msg);
+    throw new Error(msg);
   }
-  const project = projectManager.getProject(message.workspaceFolder);
-  if (!project) {
-    logger.error('Project not found');
-    return;
-  }
-  for (const key of message.config.keys()) {
-    if (key === 'selectedKitPath') {
-      const selectedKitPath = coreAPI?.getValue<string>(
-        message.workspaceFolder,
-        'selectedKitPath'
-      );
-      if (selectedKitPath !== project.selectedKitPath) {
-        await project.setSelectedKitPath(selectedKitPath);
-      }
-      continue;
-    }
-    if (key === 'selectedQtPaths') {
-      const selectedQtPaths = coreAPI?.getValue<string>(
-        message.workspaceFolder,
-        'selectedQtPaths'
-      );
-      if (selectedQtPaths !== project.qtpathsExe) {
-        await project.setQtPathsExe(selectedQtPaths);
-      }
-      continue;
-    }
-    if (key === 'workspaceType') {
-      project.workspaceType = coreAPI?.getValue<QtWorkspaceType>(
-        message.workspaceFolder,
-        'workspaceType'
-      );
-    }
-  }
+}
+
+async function initProjectManager(context: vscode.ExtensionContext) {
+  projectManager = new UIProjectManager(context);
+  await projectManager.init();
 }
