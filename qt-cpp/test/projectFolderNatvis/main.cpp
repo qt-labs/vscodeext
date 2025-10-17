8ef7227d5196e36c07f0c14c60116e45c27f9c81
Lucie Gérard <lucie.gerard@qt.io>
2025-10-17 17:14:25 +0200

    qt-cpp:natvis-test: Refactor and start debugging
    Stopping at break point.
    Locals retrieved.
    Natvis file is not read yet.

--- qt-cpp/test/projectFolderNatvis/main.cpp
+++ qt-cpp/test/projectFolderNatvis/main.cpp
@@ -1,5 +1,6 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QString>
+#include <QtCore/QRect>
 #include <iostream>
 
 //#include "core_types.h"
@@ -8,7 +9,11 @@ int main(int argc, char** argv) {
   QCoreApplication app(argc, argv);
 
   //auto coreTypes = CoreTypes();
-  // BREAK_HERE
+  // QPoint qPoint = QPoint(24, 48);
+  QRect qRect = QRect(5, 5, 42, 42);
+  QByteArray qByteArray = QByteArray("Hello World!");
+  QString qString = QString("Hello World!");
   std::cout << QString("Hello Qt World").toStdString() << std::endl;
+  // BREAK_HERE
   return 0;
 }
