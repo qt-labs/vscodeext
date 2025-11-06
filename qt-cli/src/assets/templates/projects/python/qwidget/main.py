002b492c0d92177dde4bfcc990e7368013587544
Ben Cho <ben.cho@qt.io>
2025-11-06 21:00:03 +0100

    qt-cli: Introduce `options` field in template definition
    For some templates, like the C++ class template,
    it's difficult to create a single set of template files that produce
    consistently formatted output for various parameters,
    while keeping the template files themselves readable and maintainable.
    
    In particular, controlling leading whitespace or consecutive empty lines
    across different parameters can be challenging.
    
    Adding options to polish the final output before saving to disk helps
    template authors ensure consistent formatting.
    Two new options, `polish.trimStart` and `polish.compressEmptyLines`,
    have been added to control file output formatting.

--- qt-cli/src/assets/templates/projects/python/qwidget/main.py
+++ qt-cli/src/assets/templates/projects/python/qwidget/main.py
@@ -1,14 +1,15 @@
 # This Python file uses the following encoding: utf-8
 import sys
+
 from PySide6.QtWidgets import QApplication, {{ .baseClass }}
 
 {{- if .useForm }}
+
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py
 from ui_form import Ui_{{ .className }}
 {{- end }}
-
 
 class {{ .className }}({{ .baseClass }}):
     def __init__(self, parent=None):
@@ -22,6 +23,8 @@ class {{ .className }}({{ .baseClass }}):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
     widget = {{ .className }}()
     widget.show()
+
     sys.exit(app.exec())
