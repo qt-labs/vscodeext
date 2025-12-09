403e0fa9f9d05b630955a729aafea28a40634fde
Lucie Gérard <lucie.gerard@qt.io>
2025-12-09 12:06:24 +0100

    qt-cpp:natvis-test: add container types, sample

--- qt-cpp/test/projectFolderNatvis/main.cpp
+++ qt-cpp/test/projectFolderNatvis/main.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 
 #include "core_types.h"
+#include "container_types.h"
 
 int main(int argc, char** argv) {
   QCoreApplication app(argc, argv);
 
   auto coreTypes = CoreTypes();
+  auto containerTypes = ContainerTypes();
   // BREAK_HERE
   return 0;
 }
