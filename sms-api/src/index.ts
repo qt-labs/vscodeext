283862225333b53ef90c91b26d02facf9c13eb46
Orkun Tokdemir <orkun.tokdemir@qt.io>
2026-03-12 15:15:31 +0100

    sms-api: initial commit

---
+++ sms-api/src/index.ts
@@ -0,0 +1,38 @@
+/* Copyright (C) 2026 The Qt Company Ltd.
+ *
+ * SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+ */
+
+// Types & enums
+export {
+  ErrorCategory,
+  ErrorCode,
+  InstallState,
+  UserPromptType,
+  JobStatus,
+  SessionState,
+  IPC
+} from './types';
+export type {
+  SmsError,
+  PackageReference,
+  PackageData,
+  PackageUpdate,
+  PackageFilters,
+  PackageRequestOptions,
+  UserPrompt,
+  UserPromptReply,
+  ProgressInfo,
+  JobCallbacks
+} from './types';
+
+// Transport (for advanced / testing use)
+export { PacketReader, encodePacket, encodeJsonPacket, connectSocket } from './transport';
+export type { DecodedPacket, TransportOptions } from './transport';
+
+// JSON-RPC dispatcher (for advanced use)
+export { JsonRpcDispatcher } from './jsonrpc';
+
+// Public API
+export { Session, Packages } from './client';
+export type { SessionEvents } from './client';
