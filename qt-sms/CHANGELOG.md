df7265789387bd39ade8828eb636e680c055c947
Orkun Tokdemir <orkun.tokdemir@qt.io>
2026-04-08 17:25:29 +0200

    qt-sms: Introduce Qt Software Management Service extension

---
+++ qt-sms/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+## 1.15.0 (Apr 8, 2026)
+
+🎉 **Added**
+
+- Initial release
+- Search available Qt packages
+- List installed Qt packages
+- Install Qt packages
+- Configurable service executable path
---
+++ qt-sms/src/constants.ts
@@ -0,0 +1,6 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only
+
+export * from 'qt-lib/src/constants';
+export const EXTENSION_ID = 'qt-sms';
+export const CONF_SERVICE_EXECUTABLE_PATH = 'serviceExecutablePath';
