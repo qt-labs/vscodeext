92e391e690342cab13d2548d137aa351a4744619
Orkun Tokdemir <orkun.tokdemir@qt.io>
2026-05-08 15:54:02 +0200

    qt-sms: sms-api: Add cancellable download phase
    - Split the install progress notification into two phases: a cancellable
      "Downloading" notification and a non-cancellable "Installing" one
    - Add Packages.cancel() method to sms-api (packages/cancel IPC)
    - Pass --no-console-log flag when spawning the SMS service
    - Update backend URL to `api.install.qt.io`

--- qt-sms/src/constants.ts
+++ qt-sms/src/constants.ts
@@ -7,7 +7,6 @@ export const CONF_SERVICE_EXECUTABLE_PATH = 'serviceExecutablePath';
 export const CONF_INSTALLATION_PATH = 'installationPath';
 export const CONF_USER_AGENT = 'qt-visual-studio-code-gui';
 export const CONF_RESET_LICENSE_AFTER_INSTALL = 'resetLicenseBeforeInstall';
-export const DEFAULT_BACKEND_URL =
-  'https://xck710lg2g.execute-api.eu-west-1.amazonaws.com';
+export const DEFAULT_BACKEND_URL = 'https://api.install.qt.io';
 export const ALPHA_ALLOWLIST_URL =
   'https://ordp.qt.io/install/public/vscode/60acc3e9e59df98a139e6346f00e02a25fcba5ce7ad91d67a073243f5695c4e3.txt';
