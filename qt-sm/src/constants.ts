a440272864659c6821361c0316bceda563869cfa
Orkun Tokdemir <orkun.tokdemir@qt.io>
2026-06-25 17:10:40 +0300

    qt-sm: Restrict alpha access to @qt.io emails
    Replace the remote allowlist fetch with a simple check that the Qt
    Account email ends with @qt.io, dropping the network round-trip and
    the allowlist file dependency.

--- qt-sm/src/constants.ts
+++ qt-sm/src/constants.ts
@@ -11,8 +11,6 @@ export const CONF_RESET_LICENSE_AFTER_INSTALL = 'resetLicenseBeforeInstall';
 // read it via workspace.getConfiguration('qt-sm').get('getStartedDone').
 export const CONF_GET_STARTED_DONE = 'getStartedDone';
 export const DEFAULT_BACKEND_URL = 'https://api.install.qt.io';
-export const ALPHA_ALLOWLIST_URL =
-  'https://ordp.qt.io/install/public/vscode/60acc3e9e59df98a139e6346f00e02a25fcba5ce7ad91d67a073243f5695c4e3.txt';
 
 // Survey popup constants
 export const SURVEY_URL = 'https://www.surveymonkey.com/r/BMQH2W3';
