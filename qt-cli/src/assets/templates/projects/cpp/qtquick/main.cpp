0e3a8358113f4b0ca113dd4735c53adf0902d828
Ben Cho <ben.cho@qt.io>
2026-07-01 11:45:56 +0200

    qt-cli: Update Qt Quick application templates
    Update Qt Quick application templates for both C++ and Python to align with
    changes from Qt Creator 20 release.
    
    - Update QML file template according to Qt Creator 20
      https://codereview.qt-project.org/c/qt-creator/qt-creator/+/740647
    
    - Update C++ project minimum Qt version
      - Drop Qt 6.2 support and add Qt 6.10 support.
      - Supported Qt versions: 6.5, 6.8 and 6.10.
      - Update CMakeLists.txt and main.cpp accordingly
    
    - Update files for e2e testing.
    
    Task-number: [VSCODEEXT-321](https://qt-project.atlassian.net/browse/VSCODEEXT-321)

--- qt-cli/src/assets/templates/projects/cpp/qtquick/main.cpp
+++ qt-cli/src/assets/templates/projects/cpp/qtquick/main.cpp
@@ -1,4 +1,3 @@
-{{- $mininumQtVersionFloat := (.minimumQtVersion | Qt.ParseFloat) -}}
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
@@ -7,19 +6,6 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-{{- if lt $mininumQtVersionFloat 6.5 }}
-    const QUrl url(QStringLiteral("qrc:/{{ .name }}/Main.qml"));
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreated,
-        &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
-    engine.load(url);
-{{- else }}
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
@@ -27,7 +13,6 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.loadFromModule("{{ .name }}", "Main");
-{{- end }}
 
     return app.exec();
 }
